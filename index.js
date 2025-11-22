6cc249172254ef1689ed17eeb376bb8a51f56633
Milan Srinivas <160320252+WATERMELONBOSS@users.noreply.github.com>
2025-11-22 11:36:51 -0500

    index.js in the root directory changeda nd .env file added

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
     origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
